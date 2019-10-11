59a3a3754d736638d0e87e70491405b1ff72c762
He Dian <356839046@qq.com>
2019-10-11 13:56:25 +0800

    支付测试

--- src/views/order/OrderDetail.vue
+++ src/views/order/OrderDetail.vue
@@ -63,16 +63,16 @@
     <div class="footer-bar">
       <router-link v-if="detail.status!=0" :to="{name:'order_refund',params:{id:12}}">
         <div class="btn-youya-o">
-            申请退款
+          申请退款
         </div>
       </router-link>
-      <router-link  v-if="false" :to="{name:'order_refund_detail',params:{id:12}}">
+      <router-link v-if="false" :to="{name:'order_refund_detail',params:{id:12}}">
         <div class="btn-youya-o">
-            退款详情
+          退款详情
         </div>
       </router-link>
       <div v-if="detail.status==0" class="btn-youya-o">取消订单</div>
-      <div v-if="detail.status==0" class="btn-youya" @click="payed">付款</div>
+      <div v-if="detail.status==0" class="btn-youya" @click="pay">付款</div>
       <div v-if="detail.status==2" class="btn-youya">确认收货</div>
     </div>
   </div>
@@ -84,15 +84,21 @@
     props: {},
     data() {
       return {
-        detail:{}
+        detail: {}
       };
     },
     watch: {},
     computed: {},
     methods: {
-      async getData(){
-        this.$toast.loading({message:"加载中..."})
-        let {code,data,messege} = await axios.get(`/user/mall-order/detail?id=${this.$route.params.id}`);
+      async getData() {
+        this.$toast.loading({
+          message: "加载中..."
+        })
+        let {
+          code,
+          data,
+          messege
+        } = await axios.get(`/user/mall-order/detail?id=${this.$route.params.id}`);
         if (code == 0) {
           this.$toast.clear()
           this.detail = data
@@ -100,7 +106,7 @@
           this.$toast.fail(message)
         }
       },
-      async payed(){
+      async payed() {
         this.$toast.loading({
           message: '支付中...'
         });
@@ -119,8 +125,60 @@
         }
       }
     },
+    async getSDK() {
+      let href = encodeURIComponent(window.location.href)
+      let {
+        data,
+        code,
+        message
+      } = await axios.get('/config/jsjdk?url=' + href)
+      if (code == 0) {
+        wx.config({
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: data.appId, // 必填，公众号的唯一标识
+          timestamp: Number(data.timestamp), // 必填，生成签名的时间戳
+          nonceStr: data.nonceStr, // 必填，生成签名的随机串
+          signature: data.signature, // 必填，签名，见附录1
+          jsApiList: [
+            'chooseWXPay',
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage', //1.0分享到朋友圈
+            'updateAppMessageShareData', //1.4 分享到朋友
+            'updateTimelineShareData'
+          ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        })
+      } else {
+        // $weui.topTips(message, 3000);
+      }
+    },
+    async pay() {
+      let {
+        data,
+        code,
+        message
+      } = await axios.get("/user/mall-order/pay?order_id=" + this.$route.params.id)
+      if (code == 0) {
+        wx.chooseWXPay({
+          appId: data.appId,
+          timestamp: data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+          nonceStr: data.nonceStr, // 支付签名随机串，不长于 32 位
+          package: data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+          signType: data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+          paySign: data.paySign, // 支付签名
+          success: (res) => {
+            this.$toast.success("支付成功");
+            this.$router.replace("/feedback")
+          },
+          fail: (res) => {
+            this.$toast.fail('支付失败');
+            // alert(JSON.stringify(res))
+          }
+        })
+      }
+    },
     created() {
       this.getData()
+      this.getSDK()
     },
     mounted() {}
   };
@@ -142,13 +200,15 @@
       height: 100px;
     }
   }
-  .footer-bar{
+
+  .footer-bar {
     display: flex;
     justify-content: flex-end;
     align-items: center;
     padding: 0 10px;
-    border-top:1px solid #eee;
+    border-top: 1px solid #eee;
   }
+
   .address-item {
     padding: 15px;
     display: flex;
@@ -163,30 +223,36 @@
       flex: 1;
     }
   }
-  .goods-item{
+
+  .goods-item {
     margin-bottom: 10px;
     display: flex;
-    .thumb{
+
+    .thumb {
       width: 90px;
       height: 90px;
     }
   }
-  .btn-youya,.btn-youya-o{
+
+  .btn-youya,
+  .btn-youya-o {
     margin-left: 5px;
-    width:85px;
-    height:27px;
+    width: 85px;
+    height: 27px;
     line-height: 27px;
-    border-radius:14px;
+    border-radius: 14px;
     box-sizing: border-box;
     text-align: center;
-    font-size:13px;
+    font-size: 13px;
   }
-  .btn-youya{
-    background:linear-gradient(143deg,rgba(157,195,230,1) 0%,rgba(131,179,219,1) 100%);
+
+  .btn-youya {
+    background: linear-gradient(143deg, rgba(157, 195, 230, 1) 0%, rgba(131, 179, 219, 1) 100%);
     color: #fff;
   }
-  .btn-youya-o{
-    border:1px solid #999;
+
+  .btn-youya-o {
+    border: 1px solid #999;
     color: #999;
   }
 </style>
