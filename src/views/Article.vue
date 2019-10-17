3c1959745471be24d3e0ec775f1b00f89481a01d
He Dian <356839046@qq.com>
2019-10-17 13:49:56 +0800

    update

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -76,8 +76,8 @@
           let shareData = {
             title: this.detail.share_title,
             desc: this.detail.share_subtitle, //这里请特别注意是要去除html
-            link: window.location.href,
-            imgUrl: this.detail.share_image || "http://localhost:8080/youya-h5/img/logo.png"
+            link: `http://youya.chuncom.com/youya-h5/article?id=${this.$route.query.id}`,
+            imgUrl: this.detail.share_image || "http://youya.chuncom.com/youya-h5/img/logo.png"
           }
           if (wx.onMenuShareAppMessage) { //微信文档中提到这两个接口即将弃用，故判断
             wx.onMenuShareAppMessage(shareData); //1.0 分享到朋友
@@ -88,6 +88,7 @@
           }
           wx.error(function(res) {
             // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+            alert("errorMSG:" + res)
             console.log("errorMSG:" + res);
           });
         })
@@ -97,6 +98,7 @@
       let userinfo = JSON.parse(localStorage.getItem("userinfo"));
       this.share_id = userinfo.id;
       this.getData()
+      this.getSDK()
     },
     mounted() {}
   };
