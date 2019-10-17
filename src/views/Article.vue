92c03481e90a2932ffac6a43a874f8a6229fbfe1
He Dian <356839046@qq.com>
2019-10-17 14:05:16 +0800

    update

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -88,7 +88,7 @@
           }
           wx.error(function(res) {
             // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-            alert("errorMSG:" + res)
+            // alert("errorMSG:" + res)
             console.log("errorMSG:" + res);
           });
         })
